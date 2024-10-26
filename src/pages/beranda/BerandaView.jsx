7788db7e65a3fb7310913df6b5a68881b348e1a6
suropatinyel <dracohamka@gmail.com>
2024-10-26 11:04:22 +0700

    hamka

--- src/pages/beranda/BerandaView.jsx
+++ src/pages/beranda/BerandaView.jsx
@@ -3,46 +3,6 @@ import { Link } from "react-router-dom";
 const BerandaView = ({ movies, popular, topRate, upComing }) => {
   return (
     <div className="bg-cyan-900 dark:bg-black">
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
-          />
-        </div>
-        <div id="item2" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="w-full"
-          />
-        </div>
-        <div id="item3" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="w-full"
-          />
-        </div>
-        <div id="item4" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="w-full"
-          />
-        </div>
-      </div>
-      <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-        <a href="#item4" className="btn btn-xs">
-          4
-        </a>
-      </div>
       <h1 className="text-left text-3xl font-bold pl-5 pb-2 text-cyan-400 dark:text-cyan-700">
         Now Playing
       </h1>
